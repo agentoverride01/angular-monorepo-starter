5a66c91d6dc4fb2e875ecb9f343e3c8df2ed2e29
agentoverride01 <a27998599@gmail.com>
2024-06-03 13:16:54 +0800

    added tests and coverage

--- packages/tests/src/components/button.test.ts
+++ packages/tests/src/components/button.test.ts
@@ -1,8 +1,18 @@
-
-import { ButtonComponent } from '@mono/components/button' 
+import { TestBed } from '@angular/core/testing'
+import { ButtonModule, ButtonComponent } from '@mono/components/button' 
 
 describe('Button', () => {
 
-  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ ButtonModule ],
+    }).compileComponents()
+  })
+
+  it('should create button component', () => {
+    const fixture = TestBed.createComponent(ButtonComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
 
 })
