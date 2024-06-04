dbe1b3ee1ffa3059c4cf0f30593ca56f53195950
agentoverride01 <a27998599@gmail.com>
2024-06-04 10:16:37 +0800

    added table

--- src/app/pages/powersearch/mydeal/mydeal.component.ts
+++ src/app/pages/powersearch/mydeal/mydeal.component.ts
@@ -6,7 +6,52 @@ import { Component } from '@angular/core'
     <ma-card>
       <ma-header>myDeal</ma-header>
       <ma-content>
-        <section></section>
+        <section>
+          <ul>
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li>
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li>
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li>
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li>
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li>
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li> 
+            <li>
+              <div></div>
+              <div>Coca Cola</div>
+              <div>US$ 1.28</div>
+              <div>09-Aug-2022</div>
+            </li>               
+          </ul>
+        </section>
       </ma-content>
     </ma-card>    
   `,
